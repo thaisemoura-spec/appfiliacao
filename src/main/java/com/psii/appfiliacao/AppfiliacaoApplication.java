0785d576b91106eabd3c960905d55884df8a455f
Thais Emanuelle de Moura <thais_e_moura@estudante.sesisenai.org.br>
2025-10-06 14:49:37 -0300

    Commit_01
    Atualizando as propriedades aplicadas.

---
+++ src/main/java/com/psii/appfiliacao/AppfiliacaoApplication.java
@@ -0,0 +1,13 @@
+package com.psii.appfiliacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfiliacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfiliacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psii/appfiliacao/AppfiliacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psii.appfiliacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfiliacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
